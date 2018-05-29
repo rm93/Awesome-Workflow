a6e6d49c111dd20772f69894ad6a41abd2d8e4d1
rm93 <rmedemblik93@gmail.com>
2018-05-29 11:21:44 +0200

    Open fix

--- Snakefile
+++ Snakefile
@@ -1,6 +1,6 @@
 rule open_file:
 	input:
-		"data/RNA-Seq-counts.txt"
+		"data/RNA-Seq-counts_fix.txt"
 	output:
 		"processing/gene_ids.txt"
 	script:
--- scripts/open.py
+++ scripts/open.py
@@ -1,11 +1,11 @@
 first = True
-with open(snakemake.output[0], "w") as fin:
+with open(snakemake.input[0], "r") as fin:
     with open(snakemake.output[0], "w") as fout:
-        for line in in:
+        for line in fin:
             if line[0] != "#" and line != "":
                 if first:
                     first = False
                 else:
-                    out.write(line.split("\t")[0])
+                    fout.write(line.split("\t")[0]+"\n")
 fin.close()
 fout.close()
