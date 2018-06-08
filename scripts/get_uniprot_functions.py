bf84e6b796856d999a3c051296285101b4404f3c
rm93 <snorkelen_123fan@live.nl>
2018-06-08 20:52:46 +0200

    Add comments to get_uniprot_functions.py

--- scripts/get_uniprot_functions.py
+++ scripts/get_uniprot_functions.py
@@ -1,8 +1,10 @@
 from Bio import SeqIO
 import urllib.request
 
+#Open output file
 file = open(snakemake.output[0], "w")
 
+#Get the xml files from uniprot and parse on the fly and write the results to the output file.
 with open(snakemake.input[0], "r") as input_file:
     for line in input_file:
         handle = urllib.request.urlopen("http://www.uniprot.org/uniprot/"+line.replace("\n","")+".xml")
